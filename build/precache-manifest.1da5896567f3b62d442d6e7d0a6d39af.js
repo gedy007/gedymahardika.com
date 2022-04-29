3e373d4a266c591a0e459d0585a29e772f6b71bd
gedy007 <kirimajakeemail@gmail.com>
2022-04-29 22:59:46 +0700

    deploy

---
+++ build/precache-manifest.1da5896567f3b62d442d6e7d0a6d39af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21f6db44e7c277e806d11cf484a38d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db7d62dcf8902ecf9b0",
+    "url": "/static/css/main.01dd8fc6.chunk.css"
+  },
+  {
+    "revision": "db72e7dcc1da06a0e79b",
+    "url": "/static/js/2.4eb410be.chunk.js"
+  },
+  {
+    "revision": "1db7d62dcf8902ecf9b0",
+    "url": "/static/js/main.2b1ded1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f222baba935ce2346e3772fb34b2c51c",
+    "url": "/static/media/bg.f222baba.jpg"
+  }
+]);
