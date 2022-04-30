1d4bd28dde11848628a88fa09bbe179332b2e894
gedy007 <kirimajakeemail@gmail.com>
2022-04-30 10:14:16 +0700

    Page redirection

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/:slug*',
+        source: '/en/:slug*',
+        destination: '/', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: '/id/:slug*',
         destination: '/', // Matched parameters can be used in the destination
         permanent: true,
       },
